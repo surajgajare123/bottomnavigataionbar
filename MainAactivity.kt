e99fce71a766aec32a9830051ad27bd70e88e9ea
Suraj <108755893+surajgajare123@users.noreply.github.com>
2024-06-08 10:50:21 +0530

    Create MainAactivity.kt

---
+++ MainAactivity.kt
@@ -0,0 +1,25 @@
+         bottomBar = {
+                                BottomAppBar {
+
+                                }
+                        NavigationBar {
+                            IconButton(onClick = {  }) {
+                                Icon(Icons.Rounded.Home, contentDescription ="home", modifier = Modifier.size(200.dp) )
+
+                            }
+                            IconButton(onClick = {  }) {
+                                Icon(Icons.Rounded.Favorite, contentDescription ="fav", modifier = Modifier.fillMaxWidth().size(200.dp) )
+
+                            }
+                            IconButton(onClick = {  }) {
+                                Icon(Icons.Rounded.Info, contentDescription ="info", modifier = Modifier.fillMaxWidth().size(200.dp) )
+
+                            }
+                            IconButton(onClick = {  }) {
+                                Icon(Icons.Rounded.Settings, contentDescription ="setting ", modifier = Modifier.fillMaxWidth().size(200.dp) )
+
+                            }
+
+
+                        }
+                    },
